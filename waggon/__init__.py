40177795fd294980e2b1e3593f3de269163f164a
Tigran <ramazyant@gmail.com>
2024-07-26 15:50:52 +0300

    init pys

---
+++ waggon/__init__.py
@@ -0,0 +1,10 @@
+from .utils import PRIMES, get_olhs_num, save_results, load_results, single_plot, plot_results
+
+__all__ = [
+    'PRIMES',
+    'get_olhs_num',
+    'save_results',
+    'load_results',
+    'single_plot',# TODO: change to display
+    'plot_results'
+]
---
+++ waggon/functions/__init__.py
@@ -0,0 +1,11 @@
+from .test_functions import three_hump_camel, rosenbrock, ackley, levi, himmelblau, tang, holder
+
+__all__ = [
+    'three_hump_camel',
+    'rosenbrock',
+    'ackley',
+    'levi',
+    'himmelblau',
+    'tang',
+    'holder'
+]
