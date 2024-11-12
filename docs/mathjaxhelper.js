7034ebd7a4d37801983fc0b81e8d73dd6f5136c3
Tigran <ramazyant@gmail.com>
2024-11-12 16:19:41 +0300

    docs fix

--- docs/mathjaxhelper.js
+++ docs/mathjaxhelper.js
@@ -1,20 +1,40 @@
+// window.MathJax = {
+//     tex2jax: {
+//       inlineMath: [ ["$","$"], ["\\(","\\)"] ],
+//       displayMath: [ ["$$","$$"], ["\\[","\\]"] ]
+//     },
+//     TeX: {
+//       TagSide: "right",
+//       TagIndent: ".8em",
+//       MultLineWidth: "85%",
+//       equationNumbers: {
+//         autoNumber: "AMS",
+//       },
+//       unicode: {
+//         fonts: "STIXGeneral,'Arial Unicode MS'"
+//       }
+//     },
+//     displayAlign: "center",
+//     showProcessingMessages: false,
+//     messageStyle: "none"
+//   };
+
 window.MathJax = {
-    tex2jax: {
-      inlineMath: [ ["$","$"], ["\\(","\\)"] ],
-      displayMath: [ ["$$","$$"], ["\\[","\\]"] ]
-    },
-    TeX: {
-      TagSide: "right",
-      TagIndent: ".8em",
-      MultLineWidth: "85%",
-      equationNumbers: {
-        autoNumber: "AMS",
-      },
-      unicode: {
-        fonts: "STIXGeneral,'Arial Unicode MS'"
-      }
-    },
-    displayAlign: "center",
-    showProcessingMessages: false,
-    messageStyle: "none"
-  };
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
