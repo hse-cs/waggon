a8809af5f65b77ab80af0cb0913ed1d71fc6f604
Artem Liakhov <Liakhov.Artem@yandex.ru>
2024-12-10 17:19:18 +0300

    Fix and improve Differential Evolution implementation: 1) Fix bug with using of create_candidates method from base class 2) Add evolution results logging

--- waggon/optim/evolutions.py
+++ waggon/optim/evolutions.py
@@ -29,22 +29,40 @@ class DifferentialEvolutionOptimizer(Optimiser):
         self.lhs_seed     = kwargs.get('lhs_seed', None)
         self.verbose      = kwargs.get('verbose', 1)
         self.save_results = kwargs.get('save_results', True)
-        self.candidates   = self.create_candidates(self.n_candidates)
+        self.candidates   = self.create_candidates()
 
         # Evolution parameters
         self.mutation_rate  = kwargs.get('mutation_rate', 1.0)
         self.crossover_rate = kwargs.get('crossover_rate', 0.5)
 
         # Experiments parameters
-        self.errors = []
+        self.errors = None
         self.res = None
-        self.res = None
+        self.params = None
 
-    def predict(self, *args, **kwargs) -> np.ndarray:
+    def predict(self, X=None, y=None) -> np.ndarray:
+        """
+        Make one step of evolution and returns next best parameters.
+
+        Parameters
+        ----------
+        X: Any, default = None
+            Ignored
+        y: Any, default = None
+            Ignored
+        
+        Returns
+        -------
+        x_next: np.ndarray shape of [dim, ]
+        """
+        self.evolution_step()
         y_func = self.func(self.candidates).flatten() # shape of [n_candidates]
         return self.candidates[np.argmin(y_func)]
     
     def evolution_step(self):
+        """
+        Make evolution step: mutation, crossover, selecting.
+        """
         X = self.candidates
         rng = np.random.default_rng()
 
@@ -80,10 +98,10 @@ class DifferentialEvolutionOptimizer(Optimiser):
         )
 
     def optimise(self, X=None, y=None, **kwargs):
+
         if X is None:
-            self.candidates = self.create_candidates(self.n_candidates)
-        else:
-            self.candidates = X
+            X = self.create_candidates()
+        self.candidates = X
         
         if self.verbose == 0:
             opt_loop = range(self.max_iter)
@@ -94,37 +112,20 @@ class DifferentialEvolutionOptimizer(Optimiser):
                 leave=True, 
                 position=0
             )
-        
-        self.errors = []
-        
 
         for _ in opt_loop:
-            # TO DO: add res and params logging
-            self.evolution_step()
+            self.predict()
+            x_min, y_min, error = self.evaluate()
 
-            x_cand = self.candidates
-            y_pred = self.func(x_cand).flatten() # works only for 1-d output
-            
-            x_glob_min = self.func.glob_min
-            y_glob_min = self.func(self.func.glob_min)
-
-            if self.error_type == 'x':
-                error = np.min(
-                    np.linalg.norm(x_glob_min - x_cand, axis=-1),
-                    axis = -1
-                )
-            elif self.error_type == "f":
-                error = np.min(
-                    np.linalg.norm(y_glob_min - y_pred, axis=-1),
-                    axis = -1
-                )
-            else:
-                raise ValueError(f"Unsupported error type: {self.error_type}")
-            
-            self.errors.append(error)
+            # Log metrics            
+            self.log_metrics(
+                x_min = x_min,
+                y_min = y_min,
+                error = error
+            )
 
             if error <= self.eps:
-                print('Experiment finished successfully')
+                print(f'Experiment finished successfully {self.eps}')
                 break
         
         if error > self.eps:
@@ -132,3 +133,66 @@ class DifferentialEvolutionOptimizer(Optimiser):
         
         if self.save_results:
             self._save()
+
+    def evaluate(self) -> tuple[np.ndarray, np.ndarray, float]:
+        x_cand = self.candidates
+        y_pred = self.func(x_cand).flatten() # works only for 1-d output
+
+        dim = self.func.dim
+        x_glob_min = self.func.glob_min
+        y_glob_min = self.func(self.func.glob_min.reshape(1, dim)).flatten()
+
+        if self.error_type == 'x':
+            error = np.min(
+                np.linalg.norm(x_glob_min - x_cand, axis=-1),
+                axis = -1
+            )
+        elif self.error_type == "f":
+            error = np.min(
+                np.linalg.norm(y_glob_min - y_pred, axis=-1),
+                axis = -1
+            )
+        else:
+            raise ValueError(f"Unsupported error type: {self.error_type}")
+    
+        x_min = x_cand[np.argmin(y_pred)]
+        y_min = np.min(y_pred)
+
+        return x_min, y_min, error
+    
+    def log_metrics(self, **kwargs) -> None:
+        """
+        Save optimization results
+        """
+        error = kwargs.pop('error')
+
+        if self.errors is None:
+            self.errors = []
+        self.errors.append(error)
+
+        x_min = kwargs.pop('x_min')
+        y_min = kwargs.pop('y_min')
+
+        if self.params is None:
+            dim = self.func.dim
+            self.params = np.zeros((0, dim))
+            self.res = np.zeros((0, 1))
+            
+            x_last = None
+            y_last = np.inf
+        else:
+            x_last = self.params[-1]
+            y_last = self.res[-1, 0]
+
+
+        if y_last <= y_min:
+            x_best, y_best = x_last, y_last
+        else:
+            x_best, y_best = x_min, y_min
+        
+        self.params = np.concatenate((
+            self.params, np.expand_dims(x_best, 0)
+        ))
+        self.res = np.concatenate((
+            self.res, np.expand_dims([y_best], 0)
+        ))
