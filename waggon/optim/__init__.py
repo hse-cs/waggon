f316849767f09fc21803e3d30bbb66fe4325fdce
ipere <iperederin@icloud.com>
2025-01-16 15:20:21 +0300

    Fixed problems of optimization of multidimensional functions

--- waggon/optim/__init__.py
+++ waggon/optim/__init__.py
@@ -1,9 +1,7 @@
 from .base import Optimiser
 from .optim import SurrogateOptimiser
-from .baselines.protes import ProtesOptimiser
 
 __all__ = [
     'Optimiser',
-    'SurrogateOptimiser',
-    'ProtesOptimiser'
+    'SurrogateOptimiser'
 ]
