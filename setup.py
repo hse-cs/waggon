579640bb43765d923e59269b3b27388b2e84dfed
Tigran <ramazyant@gmail.com>
2025-01-30 22:19:40 +0300

    012

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="waggon",
-    version="0.1.0",
+    version="0.1.2",
     description="WAsserstein Global Gradient-free OptimisatioN (WAGGON) methods library.",
     packages=find_packages(),
     long_description=long_description,
@@ -21,9 +21,9 @@ setup(
     ],
     install_requires=["GPy==1.13.2", "gpytorch==1.10.0", "matplotlib==3.7.5", "numpy==1.26.0",
                       "scikit_learn==1.3.2", "scipy==1.12.0", "setuptools==72.1.0",
-                      "torch==2.2.2", "torchbnn==1.2", "torchensemble==0.1.9", "tqdm==4.66.4"],
+                      "torch==2.2.2", "torchbnn==1.2", "torchensemble==0.1.9", "tqdm==4.66.4", "jax==0.5.0", "protes==0.3.11"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
-    python_requires=">=3.9",
+    python_requires=">=3.11",
 )
