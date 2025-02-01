b62c7ec7a5f3beaed4b64d01cf27e5cdf06810c7
ipere <iperederin@icloud.com>
2025-02-01 14:40:55 +0300

    Casted all types to float32 to avoid runtime errors

--- waggon/surrogates/dgp.py
+++ waggon/surrogates/dgp.py
@@ -24,8 +24,10 @@ class DGP(Surrogate):
         self.model.fit(X, y)
     
     def predict(self, X):
+        # print("predict in DGP(Surrogate): ", torch.tensor(X).dtype)
 
         f, std = self.model.predict(torch.tensor(X))
+
         return f, np.sqrt(std)
 
 
@@ -45,67 +47,6 @@ from gpytorch.models.deep_gps import DeepGPLayer, DeepGP
 from gpytorch.mlls import DeepApproximateMLL
 import gpytorch.mlls
 import numpy as np
-
-class ToyDeepGPHiddenLayer(DeepGPLayer):
-    # Наследуется от базового класса скрытых слоев DeepGPLayer.
-
-    def __init__(self, input_dims, output_dims, num_inducing=100, mean_type='constant'):
-        if output_dims is None:
-            # print("BEBE")
-            inducing_points = torch.randn(num_inducing, input_dims)
-            batch_shape = torch.Size([])
-        else:
-            inducing_points = torch.randn(output_dims, num_inducing, input_dims)
-            batch_shape = torch.Size([output_dims])
-
-        variational_distribution = CholeskyVariationalDistribution(
-            num_inducing_points=num_inducing,
-            batch_shape=batch_shape
-        )
-
-        variational_strategy = VariationalStrategy(
-            self,
-            inducing_points,
-            variational_distribution,
-            learn_inducing_locations=True
-        )
-
-        super(ToyDeepGPHiddenLayer, self).__init__(variational_strategy, input_dims, output_dims)
-
-        if mean_type == 'constant':
-            self.mean_module = ConstantMean(batch_shape=batch_shape)
-        else:
-            self.mean_module = LinearMean(input_dims)
-        self.covar_module = ScaleKernel(
-            RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims),
-            batch_shape=batch_shape, ard_num_dims=None
-        )
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        # print(MultivariateNormal(mean_x, covar_x))
-        return MultivariateNormal(mean_x, covar_x)
-
-    def __call__(self, x, *other_inputs, **kwargs):
-        """
-        Overriding __call__ isn't strictly necessary, but it lets us add concatenation based skip connections
-        easily. For example, hidden_layer2(hidden_layer1_outputs, inputs) will pass the concatenation of the first
-        hidden layer's outputs and the input data to hidden_layer2.
-        """
-        if len(other_inputs):
-            if isinstance(x, gpytorch.distributions.MultitaskMultivariateNormal):
-                x = x.rsample()
-
-            processed_inputs = [
-                inp.unsqueeze(0).expand(gpytorch.settings.num_likelihood_samples.value(), *inp.shape)
-                for inp in other_inputs
-            ]
-
-            x = torch.cat([x] + processed_inputs, dim=-1)
-
-        # Возвращает MultitaskMultivariateNormal distribution или MultivariateNormal distribution если output_dims=None. То есть возвращает Гауссовский вектор - многомерное нормальное распределение.
-        return super().__call__(x, are_samples=bool(len(other_inputs)))
     
 
 num_hidden_dims = 2
@@ -202,10 +143,12 @@ class DistributionalDGP(DeepGP):
 
       epochs_iter = tqdm(range(self.n_epochs), desc="Epoch") if self.verbose > 1 else range(self.n_epochs)
 
+    #   print("Fit in class Distributional GP: ", X.dtype)  
       for _ in epochs_iter:
         closs = 0
         with gpytorch.settings.num_likelihood_samples(self.num_samples):
-            for x_batch, y_batch in DataLoader(TensorDataset(torch.tensor(X), torch.tensor(y)), batch_size=self.batch_size, shuffle=True):
+            for x_batch, y_batch in DataLoader(TensorDataset(torch.tensor(X, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)), batch_size=self.batch_size, shuffle=True):
+                # print("Fit in class Distributional GP x_batch: ", x_batch.dtype)  
                 output = self(x_batch)
                 loss = -mll(output, y_batch)
                 loss.backward(retain_graph=True)
@@ -223,7 +166,7 @@ class DistributionalDGP(DeepGP):
       return self.loss_history
 
     def forward(self, x):
-        with gpytorch.settings.num_likelihood_samples(1):
+        with gpytorch.settings.num_likelihood_samples(self.num_samples):
             hidden_rep1 = self.hidden_layer(x)
             output = self.last_layer(hidden_rep1)
         # mean_x = self.mean_module(output)
@@ -236,7 +179,8 @@ class DistributionalDGP(DeepGP):
             variances = []
             # lls = []
             # for x_batch, y_batch in test_loader:
-            preds = self.likelihood(self(x))
+            # print("Distributional DGP predict; ", x.float().dtype)
+            preds = self.likelihood(self(x.float()))
             mus.append(preds.mean)
             variances.append(preds.variance)
             # lls.append(self.likelihood.log_marginal(y, self(x)))
@@ -264,91 +208,3 @@ class DistributionalDGP(DeepGP):
             # Метод call идет из базового класса Module, по умолчанию передает содержимое в forward, затем проверяет, чтобы тип выходной переменной был один из этих - (Distribution, torch.Tensor, LinearOperator)
             # Принимает значения размерности от двух
         return super().__call__(x)
-
-
-class WassersteinDGP(DistributionalDGP):
-    def __init__(self, input_dim, out_dim, num_samples=100, epochs=1500):
-        super().__init__(input_dim, out_dim, num_samples, epochs)
-        hidden_layer = ToyDeepGPHiddenLayer(
-            input_dims=input_dim,
-            output_dims=out_dim,
-            mean_type='linear',
-        )
-
-        last_layer = ToyDeepGPHiddenLayer(
-            input_dims=hidden_layer.output_dims,
-            output_dims=out_dim,
-            mean_type='ZeroMean',
-        )
-
-        self.hidden_layer = hidden_layer
-        self.last_layer = last_layer
-        self.likelihood = GaussianLikelihood()
-        self.epochs = epochs
-        self.num_samples=num_samples
-
-    def fit(self, train_mean_x : torch.Tensor, train_y_mean : torch.Tensor):
-      num_epochs = self.epochs
-    #   num_samples = self.num_samples
-      self.loss_history = []
-      self.predictions_history = []
-
-      optimizer = torch.optim.Adam([
-          {'params': self.parameters()},
-      ], lr=0.01)
-      mll = DeepApproximateMLL(VariationalELBO(self.likelihood, self, 10))
-
-
-      epochs_iter = tqdm(range(num_epochs), desc="Epoch")
-
-      for _ in epochs_iter:
-        with gpytorch.settings.num_likelihood_samples(self.num_samples):
-            optimizer.zero_grad()
-            output = self(train_mean_x)
-            # print(output, train_y_mean.shape)
-            loss = -mll(output.mean[0], train_y_mean)
-            # print(loss.shape)
-            loss.backward()
-            loss = loss.item()
-            optimizer.step()
-        self.loss_history.append(loss)
-
-        self.predictions_history.append(self.__predict(train_mean_x).mean().item())
-      return self.loss_history
-    
-    def __call__(self, x, *other_inputs, **kwargs):
-        """
-        Overriding __call__ isn't strictly necessary, but it lets us add concatenation based skip connections
-        easily. For example, hidden_layer2(hidden_layer1_outputs, inputs) will pass the concatenation of the first
-        hidden layer's outputs and the input data to hidden_layer2.
-        """
-        with gpytorch.settings.num_likelihood_samples(self.num_samples):
-            if len(other_inputs):
-                if isinstance(x, gpytorch.distributions.MultitaskMultivariateNormal):
-                    x = x.rsample()
-
-                processed_inputs = [
-                    inp.unsqueeze(0).expand(gpytorch.settings.num_likelihood_samples.value(), *inp.shape)
-                    for inp in other_inputs
-                ]
-
-                x = torch.cat([x] + processed_inputs, dim=-1)
-
-            # Метод call идет из базового класса Module, по умолчанию передает содержимое в forward, затем проверяет, чтобы тип выходной переменной был один из этих - (Distribution, torch.Tensor, LinearOperator)
-            # Принимает значения размерности от двух
-            return super().__call__(x)
-
-
-    def sample(x : torch.Tensor):
-        return x
-
-    def predict(self, x : torch.Tensor):
-        return np.sample()
-#     def Barycenter(self, dot_x, dot_y):
-        # gp_0 = (mu_0, k_0)     
-        # gp_1 = (mu_1, k_1)
-        # # mu_0/mu_1 (ndarray (n, 1)) is the mean of one Gaussian Process 
-        # # K_0/K_1 (ndarray (n, n)) is the covariance matrix of one 
-        # # Gaussain Process
-
-        # wd_gp = Wasserstein_GP(gp_0, gp_1)
