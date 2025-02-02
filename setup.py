8c6f8d1f505c32ddcf2a5da3f79909d94f8831bf
ipere <iperederin@icloud.com>
2025-02-02 17:17:57 +0300

    updated version of the lib

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="waggon",
-    version="0.1.7",
+    version="0.1.8",
     description="WAsserstein Global Gradient-free OptimisatioN (WAGGON) methods library.",
     packages=find_packages(),
     long_description=long_description,
