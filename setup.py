0388f6b9d1973bdc86291ea11eae25d9c905745c
ipere <iperederin@icloud.com>
2025-02-02 23:25:17 +0300

    Back to normal setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     install_requires=["GPy==1.13.2", "gpytorch==1.10.0", "matplotlib==3.7.5", "numpy==1.26.0",
                       "scikit_learn==1.3.2", "scipy==1.12.0", "setuptools==72.1.0",
-                      "torch==2.2.2+cu118", "torchbnn==1.2", "torchensemble==0.1.9", "tqdm==4.66.4", "jax==0.5.0", "protes==0.3.11"],
+                      "torch==2.2.2", "torchbnn==1.2", "torchensemble==0.1.9", "tqdm==4.66.4", "jax==0.5.0", "protes==0.3.11"],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
