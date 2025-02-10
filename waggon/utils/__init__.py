487d02772c4279f669d3cb810731c852d9091233
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-02-10 22:25:46 +0300

    Add context managers to set seeds

--- waggon/utils/__init__.py
+++ waggon/utils/__init__.py
@@ -1,6 +1,16 @@
-from .utils import display, plot_results
+from .utils import display
+from .utils import plot_results
+
+from .utils import set_all_seed
+from .utils import fixed_random_seed
+from .utils import fixed_numpy_seed
+from .utils import fixed_torch_seed
+from .utils import fixed_all_seed
+
 
 __all__ = [
     'display',
-    'plot_results'
+    'plot_results',
+    'set_all_seed'
 ]
+
--- waggon/utils/utils.py
+++ waggon/utils/utils.py
@@ -1,9 +1,15 @@
 import os
 import pickle
+import contextlib
+import random
+
 import numpy as np
-from waggon.optim import Optimiser, SurrogateOptimiser
+import torch
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+
+from waggon.optim import Optimiser
+from waggon.optim import SurrogateOptimiser
 
 
 def load_results_for_plotting(func_dir, acqf_name, surr_name, base_dir='test_results', exp_transform=True):
@@ -162,3 +168,99 @@ def plot_results(base_dir='test_results', figsize=(16, 8), **kwargs):
 # X,Y = np.meshgrid(x, y)
 # data = np.vstack([X.ravel(), Y.ravel()])
 # plt.contourf(X, Y, d(data.T).reshape(100, 100))
+
+
+@contextlib.contextmanager
+def fixed_random_seed(seed: int):
+    """
+    Context manager to set specified seed for random package
+
+    Parameters
+    ----------
+    seed: int
+        The seed value to set
+    """
+    state = random.getstate()
+    random.seed(seed)
+    try:
+        yield
+    finally:
+        random.setstate(state)
+
+
+@contextlib.contextmanager
+def fixed_numpy_seed(seed: int):
+    """
+    Context manager to set specified seed for NumPy
+
+    Parameters
+    ----------
+    seed: int
+        The seed value to set
+    """
+    state = np.random.get_state()
+    np.random.seed(seed)
+    try:
+        yield
+    finally:
+        np.random.set_state(state)
+
+
+@contextlib.contextmanager
+def fixed_torch_seed(seed: int):
+    """
+    Context manager to set specified for Torch
+    """
+    state = torch.get_rng_state()
+    cuda_state = torch.cuda.get_rng_state_all()
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    try:
+        yield
+    finally:
+        torch.set_rng_state(state)
+        torch.cuda.set_rng_state_all(cuda_state)
+
+
+def set_all_seed(seed: int):
+    """
+    Set specified seed for random, numpy, torch packages
+
+    Parameters
+    ----------
+    seed: int
+        The seed value to set
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+@contextlib.contextmanager
+def fixed_all_seed(seed: int):
+    """
+    Context manager to set specified seed for random, numpy and torch packages
+
+    Parameters
+    ----------
+    seed: int
+        The seed value to set
+    """
+    random_state = random.getstate()
+    numpy_state = np.random.get_state()
+    torch_state = torch.get_rng_state()
+    torch_cuda_state = torch.cuda.get_rng_state_all()
+
+    set_all_seed(seed)
+
+    try:
+        yield
+    finally:
+        random.setstate(random_state)
+        np.random.set_state(numpy_state)
+        torch.set_rng_state(torch_state)
+        torch.cuda.set_rng_state_all(torch_cuda_state)
+
