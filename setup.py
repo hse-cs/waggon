db426d3e0cb501717c5afc1c39b767f1953bf8d5
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-02-10 22:26:51 +0300

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -19,11 +19,26 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=["GPy==1.13.2", "gpytorch==1.10.0", "matplotlib==3.7.5", "numpy==1.26.0",
-                      "scikit_learn==1.3.2", "scipy==1.12.0", "setuptools==72.1.0",
-                      "torch==2.2.2", "torchbnn==1.2", "torchensemble==0.1.9", "tqdm==4.66.4", "jax==0.5.0", "protes==0.3.11"],
-    extras_require={
-        "dev": ["pytest>=7.0", "twine>=4.0.2"],
-    },
     python_requires=">=3.11",
+    install_requires=[
+        "GPy==1.13.2", 
+        "gpytorch==1.10.0", 
+        "matplotlib==3.7.5", 
+        "numpy==1.26.0",
+        "scikit_learn==1.3.2", 
+        "scipy==1.12.0", 
+        "setuptools==72.1.0",
+        "torch==2.2.2", 
+        "torchbnn==1.2", 
+        "torchensemble==0.1.9", 
+        "tqdm==4.66.4", 
+        "jax==0.5.0", 
+        "protes==0.3.11"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0", 
+            "twine>=4.0.2",
+        ],
+    }
 )
