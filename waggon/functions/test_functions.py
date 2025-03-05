058076ddecf5c4a0b132883c78e1aaa573ffea19
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-03-06 00:40:57 +0300

    Little clean up test functions code

--- waggon/functions/test_functions.py
+++ waggon/functions/test_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.linalg
+
 from .base import Function
 
 
@@ -13,7 +15,7 @@ class three_hump_camel(Function):
         self.domain   = np.array([[-5, 5], [-5, 5]])
         self.name     = 'Three Hump Camel'
         self.glob_min = np.zeros(self.dim).reshape(1, -1)
-        self.f        = lambda x: 2 * x[:, 0]**2 - 1.05 * x[:, 0]**4 + x[:, 0]**6 / 6 + x[:, 0]*x[:, 1] + x[:, 1]**2
+        self.f        = lambda x: 2.0 * x[:, 0] ** 2.0 - 1.05 * x[:, 0] ** 4.0 + x[:, 0] ** 6.0 / 6.0 + x[:, 0] * x[:, 1] + x[:, 1] ** 2
 
 
 class rosenbrock(Function):
@@ -27,7 +29,10 @@ class rosenbrock(Function):
         self.domain   = np.array([self.dim*[-2, 2]]).reshape(self.dim, 2)
         self.name     = f'Rosenbrock ({self.dim} dim.)'
         self.glob_min = np.ones(self.dim).reshape(1, -1)
-        self.f        = lambda x: np.sum(np.array([100 * (x[:, i+1] - x[:, i] ** 2)**2 + (1 - x[:, i])**2 for i in range(self.dim - 1)]), axis=0).squeeze()
+        self.f        = lambda x: np.sum(
+            100.0 * (x[..., 1:] - x[..., :-1] ** 2.0) ** 2.0 + (1 - x[..., :-1]) ** 2.0, 
+            axis=-1,
+        )
 
 
 class tang(Function):
@@ -41,7 +46,10 @@ class tang(Function):
         self.domain   = np.array([self.dim*[-5, 5]]).reshape(self.dim, 2)
         self.name     = f'Styblinski-Tang ({self.dim} dim.)'
         self.glob_min = np.ones(self.dim).reshape(1, -1) * -2.903534
-        self.f        = lambda x: np.sum(x**4 - 16*x**2 + 5*x + 39.16617*self.dim, axis=1).squeeze()
+        self.f        = lambda x: np.sum(
+            x ** 4.0 - 16.0 * x ** 2.0 + 5.0 * x + 39.16617 * self.dim, 
+            axis=-1, 
+        )
 
 
 class ackley(Function):
@@ -55,7 +63,7 @@ class ackley(Function):
         self.domain   = np.array([self.dim*[-5, 5]]).reshape(self.dim, 2)
         self.name     = 'Ackley'
         self.glob_min = np.zeros(self.dim).reshape(1, -1)
-        self.f        = lambda x: -20 * np.exp(-0.2*np.sqrt((1./self.dim) * (np.sum(x**2, axis=1)))) -  np.exp((1./self.dim) * (np.sum(np.cos(2*np.pi*x), axis=1))) + np.e + 20
+        self.f        = lambda x: -20.0 * np.exp(-0.2 * np.sqrt((1.0 / self.dim) * (np.sum(x ** 2.0, axis=1)))) -  np.exp((1.0 / self.dim) * (np.sum(np.cos(2.0 * np.pi * x), axis=1))) + np.e + 20
 
 
 class levi(Function):
@@ -69,7 +77,7 @@ class levi(Function):
         self.domain   = np.array([self.dim*[-4, 6]]).reshape(self.dim, 2)
         self.name     = 'Lévi'
         self.glob_min = np.ones(self.dim).reshape(1, -1)
-        self.f        = lambda x: (np.sin(3*np.pi*x[:, 0]))**2 + ((x[:, 0] - 1)**2) * (1 + (np.sin(3*np.pi*x[:, 1]))**2) + ((x[:, 1] - 1)**2) * (1 + (np.sin(2*np.pi*x[:, 1]))**2)
+        self.f        = lambda x: (np.sin(3 * np.pi * x[:, 0])) ** 2 + ((x[:, 0] - 1) ** 2) * (1 + (np.sin(3 * np.pi * x[:, 1])) ** 2) + ((x[:, 1] - 1) ** 2) * (1 + (np.sin(2 * np.pi * x[:, 1])) ** 2)
     
     def sample(self, x):
         X, y = None, None
@@ -103,7 +111,7 @@ class himmelblau(Function):
         self.domain   = np.array([self.dim*[-5, 5]]).reshape(self.dim, 2)
         self.name     = 'Himmelblau'
         self.glob_min = np.array([[3.0, 2.0], [-2.805118, 3.131312], [-3.779310, -3.283186], [3.584428, -1.848126]])
-        self.f        = lambda x: (x[:, 0]**2 + x[:, 1] - 11)**2 + (x[:, 0] + x[:, 1]**2 - 7)**2 
+        self.f        = lambda x: (x[:, 0]**2 + x[:, 1] - 11)**2 + (x[:, 0] + x[:, 1]**2 - 7) ** 2
 
 
 class holder(Function):
@@ -118,3 +126,32 @@ class holder(Function):
         self.name     = 'Holder'
         self.glob_min = np.array([[8.05502, 9.66459], [-8.05502, -9.66459], [-8.05502, 9.66459], [8.05502, -9.66459]])
         self.f        = lambda x: -np.abs(np.sin(x[:, 0]) * np.cos(x[:, 1]) * np.exp(np.abs(1 - np.sqrt(x[:, 0]**2 + x[:, 1]**2)/np.pi))) + 19.2085
+
+
+class submanifold_rosenbrock(Function):
+    """
+    Submanifold Rosenbrock problem function
+    """
+    def __init__(self, dim=20, sub_dim=8, **kwargs):
+        super().__init__(**kwargs)
+        self.dim = dim
+        self.sub_dim = sub_dim
+        self.domain = np.tile([-10, 10], reps=(dim, 2))
+        
+        self.name = f"Submanifold Rosenbrock (dim={dim}, subdim={sub_dim})"
+        
+        A = np.random.randn(self.dim, self.sub_dim)
+        Q, _ = np.linalg.qr(A)
+        b = np.ones(sub_dim)
+
+        print(Q.shape)
+        print(b.shape)
+
+        x_min, _, _, _ = scipy.linalg.lstsq(Q.T, b)
+
+        self.glob_min = np.expand_dims(x_min, -1)
+        self.Q = Q
+        self.f = lambda x: np.sum(
+            100 * (x @ self.Q[:, 1:] - (x @ self.Q[:, :-1])**2)**2 + (1 - (x @ self.Q[:, :-1])**2),
+            axis=-1
+        )
