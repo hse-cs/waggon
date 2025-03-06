d633f66026b4b557001fe0b5f59dd4837de0cfd4
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-03-06 15:47:46 +0300

    Add function for plotting 2d functions

--- waggon/utils/utils.py
+++ waggon/utils/utils.py
@@ -8,6 +8,7 @@ import torch
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+from waggon.functions import FunctionV2
 from waggon.optim import Optimiser
 from waggon.optim import SurrogateOptimiser
 
@@ -162,13 +163,43 @@ def plot_results(base_dir='test_results', figsize=(16, 8), **kwargs):
     fig.savefig('plots/res.jpeg', format='jpeg', dpi=200, bbox_inches='tight')
 
 
-# x = np.linspace(-5, 5, 100)
-# y = np.linspace(-5, 5, 100)
+def plot_func_2d(func, fig, ax, x_range, y_range, name=None, nx=150, ny=150, norm=None):
+    """
+    Plot colormap for 2-dimensional function
+    """
+    assert func.dim == 2
 
-# X,Y = np.meshgrid(x, y)
-# data = np.vstack([X.ravel(), Y.ravel()])
-# plt.contourf(X, Y, d(data.T).reshape(100, 100))
+    x_min, x_max = x_range
+    y_min, y_max = y_range
 
+    x_arr = np.linspace(x_min, x_max, nx)
+    y_arr = np.linspace(y_min, y_max, ny)
+
+    xs, ys = np.meshgrid(x_arr, y_arr)
+
+    x_inp = np.zeros((ny * nx, 2))
+    x_inp[:, 0] = xs.flatten()
+    x_inp[:, 1] = ys.flatten()
+
+    z_out = func(x_inp)
+    z_out = z_out.squeeze(1) if z_out.ndim == 2 else z_out
+    z_out = z_out.reshape((nx, ny), order='F')
+    z_out = np.transpose(z_out)
+
+    mesh = ax.pcolormesh(x_arr, y_arr, z_out, norm=norm)
+    cbar = fig.colorbar(mesh, ax=ax)
+
+    glob_min = func.glob_min.reshape(-1, 1)
+    ax.scatter(func.glob_min[:, 0], func.glob_min[:, 1], color='red', s=8)
+
+    if name is None:
+        title_name = f"{func.name} (new API)" if isinstance(func, FunctionV2) else f"{func.name} (old API)"
+    else:
+        title_name = name
+
+    ax.set_title(title_name)    
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
 
 @contextlib.contextmanager
 def fixed_random_seed(seed: int):
@@ -263,4 +294,3 @@ def fixed_all_seed(seed: int):
         np.random.set_state(numpy_state)
         torch.set_rng_state(torch_state)
         torch.cuda.set_rng_state_all(torch_cuda_state)
-
