691c1ea224b820d5b80d322f8ece4d1847e4da25
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-03-06 16:52:49 +0300

    Fix utils/__init__.py

--- waggon/utils/__init__.py
+++ waggon/utils/__init__.py
@@ -1,5 +1,6 @@
 from .utils import display
 from .utils import plot_results
+from .utils import plot_func_2d
 
 from .utils import set_all_seed
 from .utils import fixed_random_seed
@@ -11,6 +12,11 @@ from .utils import fixed_all_seed
 __all__ = [
     'display',
     'plot_results',
-    'set_all_seed'
+    'plot_func_2d',
+    'set_all_seed',
+    'fixed_random_seed',
+    'fixed_numpy_seed',
+    'fixed_torch_seed',
+    'fixed_all_seed',
 ]
 
