5525a94da0f92f33dd42d644ba1cd4a0f4d72f32
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-03-27 21:11:39 +0300

    Add new API functions to __init__.py

--- waggon/functions/__init__.py
+++ waggon/functions/__init__.py
@@ -1,6 +1,20 @@
 from .base import Function
 # from .nn_hyperparams import NNhyperparams
-from .test_functions import three_hump_camel, rosenbrock, ackley, levi, himmelblau, tang, holder#, submanifold, nonlinear_submanifold, optimal_control
+from .test_functions import three_hump_camel 
+from .test_functions import rosenbrock 
+from .test_functions import ackley
+from .test_functions import levi
+from .test_functions import himmelblau 
+from .test_functions import tang
+from .test_functions import holder
+from .test_functions import submanifold_rosenbrock
+
+from .base import FunctionV2
+from .landscapes.sphere import Sphere
+from .landscapes.thc import ThreeHumpCamel
+from .landscapes.ackley import Ackley
+from .landscapes.submanifold import SubmanifoldRosenbrock
+
 
 __all__ = [
     'Function',
@@ -11,8 +25,14 @@ __all__ = [
     'himmelblau',
     'tang',
     'holder',
-    # 'submanifold',
+    'submanifold_rosenbrock',
     # 'nonlinear_submanifold',
-    # 'optimal_control'
-    # 'NNhyperparams',
+    # 'optimal_control',
+    # 'NNhyperparams'
+    
+    "FunctionV2",
+    "Sphere",
+    "ThreeHumpCamel",
+    "Ackley",
+    "SubmanifoldRosenbrock",
 ]
